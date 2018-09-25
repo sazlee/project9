90bfe495599c42db4c8effba29c0393409470d42
sazlee <35682982+sazlee@users.noreply.github.com>
2018-09-25 09:35:40 +0800

    Add files via upload

---
+++ Testgithub.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgithub;
+
+/**
+ *
+ * @author User
+ */
+public class Testgithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        {
+System.out.println("Laporan Cuaca Hari Ini");
+String Lokasi="Alor Setar,Kedah";
+System.out.println("Koordinat GPS >>");
+
+double Latitude = 676775.13436347;
+double Longitude = 650506.29819875;
+System.out.print("Longitude:"+ Longitude);
+System.out.println("  Latitude:"+ Latitude);
+float suhu=88.398926f;
+boolean Banjir=false;
+System.out.println("suhu:"+suhu +"F");
+System.out.println("Amaran Banjir:"+Banjir);
+         
+}
+    }
+    
+}
